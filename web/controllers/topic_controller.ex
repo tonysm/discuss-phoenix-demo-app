0a9804fbeb571c899e83e328e56a55c42ca2b25c
Tony Messias <tonyzrp@gmail.com>
2017-08-31 21:17:21 -0300

    Initial commit

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,30 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> 
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
+  end
+end
