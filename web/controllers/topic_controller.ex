1d4a8e365b3afa0555818bacf68c46263bad7dc2
Tony Messias <tonyzrp@gmail.com>
2017-09-01 01:37:53 -0300

    Edit topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -19,12 +19,33 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, topic} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
       {:error, changeset} -> 
         render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => form}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic, form)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: topic
     end
   end
 end
